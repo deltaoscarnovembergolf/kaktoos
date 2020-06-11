910ef4421402f48ab8a840ad3426f858c0cebe64
BalintCsala <balint.csala@gmail.com>
2020-06-11 13:19:12 +0200

    Works now I guess

--- kaktoos.cu
+++ kaktoos.cu
@@ -6,21 +6,23 @@
 #include <vector>
 #include <mutex>
 #include <chrono>
+#include <string>
+#include <fstream>
 
 #define RANDOM_MULTIPLIER 0x5DEECE66DULL
 #define RANDOM_ADDEND 0xBULL
 #define RANDOM_MASK ((1ULL << 48ULL) - 1ULL)
 
 #ifndef FLOOR_LEVEL
-#define FLOOR_LEVEL 63ULL
+#define FLOOR_LEVEL 63LL
 #endif
 
 #ifndef WANTED_CACTUS_HEIGHT
-#define WANTED_CACTUS_HEIGHT 8ULL
+#define WANTED_CACTUS_HEIGHT 10LL
 #endif
 
 #ifndef WORK_UNIT_SIZE
-#define WORK_UNIT_SIZE (1ULL << 23ULL)
+#define WORK_UNIT_SIZE 2048
 #endif
 
 #ifndef BLOCK_SIZE
@@ -69,8 +71,16 @@ namespace java_random {
 
 }
 
-__global__ __launch_bounds__(256, 2) void crack(uint64_t seed_offset, int32_t *num_seeds, uint64_t *seeds) {
-    uint64_t originalSeed = blockIdx.x * blockDim.x + threadIdx.x + seed_offset;
+struct seed {
+    uint64_t seed;
+    int16_t posX;
+    int16_t posZ;
+    int32_t height;
+};
+
+__global__ __launch_bounds__(256, 2) void crack(int32_t *num_seeds, seed *seeds, const uint64_t *input_seeds) {
+    int32_t id = blockIdx.x * blockDim.x + threadIdx.x;
+    uint64_t originalSeed = input_seeds[id];
     uint64_t seed = originalSeed;
 
     int8_t heightMap[1024];
@@ -83,7 +93,7 @@ __global__ __launch_bounds__(256, 2) void crack(uint64_t seed_offset, int32_t *n
     int16_t currentHighestPos = 0;
     int16_t terrainHeight;
     int16_t initialPosX, initialPosY, initialPosZ;
-    int16_t posX, posY, posZ;
+    int16_t posX, posY, posZ, sPosX, sPosZ;
     int16_t offset, posMap;
 
     int16_t i, a, j;
@@ -122,12 +132,14 @@ __global__ __launch_bounds__(256, 2) void crack(uint64_t seed_offset, int32_t *n
 
                 if (heightMap[currentHighestPos] < heightMap[posMap]) {
                     currentHighestPos = posMap;
+                    sPosX = posX;
+                    sPosZ = posZ;
                 }
             }
         }
 
         if (heightMap[currentHighestPos] - FLOOR_LEVEL >= WANTED_CACTUS_HEIGHT) {
-            seeds[atomicAdd(num_seeds, 1)] = originalSeed;
+            seeds[atomicAdd(num_seeds, 1)] = { originalSeed, sPosX, sPosZ, (int32_t)(heightMap[currentHighestPos] - FLOOR_LEVEL) };
             return;
         }
     }
@@ -135,82 +147,54 @@ __global__ __launch_bounds__(256, 2) void crack(uint64_t seed_offset, int32_t *n
 
 struct GPU_Node {
     int* num_seeds;
-    uint64_t* seeds;
+    seed* seeds;
+    uint64_t* input_seeds;
 };
 
-void setup_gpu_node(GPU_Node* node, int32_t gpu) {
-    cudaSetDevice(gpu);
-    cudaMallocManaged(&node->num_seeds, sizeof(*node->num_seeds));
-    cudaMallocManaged(&node->seeds, 1ULL << 10ULL);
-}
+int main() {
+    printf("Searching %ld total seeds...\n", END - OFFSET);
 
-GPU_Node nodes[GPU_COUNT];
-uint64_t offset = OFFSET;
-uint64_t count = 0;
-std::mutex info_lock;
-std::vector<uint64_t> seeds;
+    std::vector<uint64_t> input;
+    std::ifstream inputFile("out.txt");
+    while (inputFile.good()) {
+        int32_t height;
+        uint64_t seed;
+        inputFile >> height;
+        inputFile >> seed;
+        inputFile.ignore(1);
+        input.push_back(seed);
+    }
+    uint64_t count = 0;
+    uint64_t offset = OFFSET;
+    printf("Seed count: %d\n", (int)input.size());
 
-void gpu_manager(int32_t gpu_index) {
-    std::string fileName = "kaktoos_seeds" + std::to_string(gpu_index) + ".txt";
-    FILE *out_file = fopen(fileName.c_str(), "a");
-    cudaSetDevice(gpu_index);
-    while (offset < END) {
-        *nodes[gpu_index].num_seeds = 0;
-        crack<<<WORK_UNIT_SIZE / BLOCK_SIZE, BLOCK_SIZE, 0>>> (offset, nodes[gpu_index].num_seeds, nodes[gpu_index].seeds);
-        info_lock.lock();
+    FILE *out_file = fopen("seeds.txt", "w");
+
+    GPU_Node node{};
+
+    cudaMallocManaged(&node.num_seeds, sizeof(*node.num_seeds));
+    cudaMallocManaged(&node.seeds, sizeof(seed) * WORK_UNIT_SIZE);
+    cudaMallocManaged(&node.input_seeds, sizeof(uint64_t) * WORK_UNIT_SIZE);
+
+    while (offset < input.size()) {
+        printf("Offset: %lu\n", offset);
+        *node.num_seeds = 0;
+
+        for (int i = 0; i < WORK_UNIT_SIZE; i++) {
+            node.input_seeds[i] = input[i + offset];
+        }
+
+        crack<<<WORK_UNIT_SIZE / BLOCK_SIZE, BLOCK_SIZE, 0>>> (node.num_seeds, node.seeds, node.input_seeds);
         offset += WORK_UNIT_SIZE;
-        info_lock.unlock();
         cudaDeviceSynchronize();
-        for (int32_t i = 0, e = *nodes[gpu_index].num_seeds; i < e; i++) {
-            fprintf(out_file, "%lld\n", (long long int)nodes[gpu_index].seeds[i]);
-            seeds.push_back(nodes[gpu_index].seeds[i]);
+        for (int32_t i = 0, e = *node.num_seeds; i < e; i++) {
+            fprintf(out_file, "%d %lld %d %d\n", node.seeds[i].height, (long long int)node.seeds[i].seed, node.seeds[i].posX - 8, node.seeds[i].posZ - 8);
+            printf("Found seed: %lld\n", (long long int)node.seeds[i].seed);
         }
         fflush(out_file);
-        info_lock.lock();
-        count += *nodes[gpu_index].num_seeds;
-        info_lock.unlock();
+        count += *node.num_seeds;
     }
     fclose(out_file);
-}
-
-int main() {
-    printf("Searching %ld total seeds...\n", (long int)(END - OFFSET));
-
-    std::thread threads[GPU_COUNT];
-
-    time_t startTime = time(nullptr), currentTime;
-    for(int32_t i = 0; i < GPU_COUNT; i++) {
-        setup_gpu_node(&nodes[i], i);
-        threads[i] = std::thread(gpu_manager, i);
-    }
-
-    using namespace std::chrono_literals;
-
-    while (offset < END) {
-        time(&currentTime);
-        int timeElapsed = (int)(currentTime - startTime);
-        double speed = (double)(offset - OFFSET) / (double)timeElapsed / 1000000.0;
-        printf("Searched %lld seeds, offset: %lld found %lld matches. Time elapsed: %ds. Speed: %.2fm seeds/s. %f%%\n",
-            (long long int)(offset - OFFSET),
-            (long long int)offset,
-            (long long int)count,
-            timeElapsed,
-            speed,
-            (double)(offset - OFFSET) / (END - OFFSET) * 100);
-
-        if (timeElapsed % 2000 == 0) {
-            printf("Backup seed list:\n");
-            for (auto &seed : seeds) {
-                printf("%llu\n", (unsigned long long) seed);
-            }
-        }
-
-        std::this_thread::sleep_for(1s);
-    }
-
-    for (auto &thread : threads) {
-        thread.join();
-    }
 
     printf("Done!\n");
     printf("But, verily, it be the nature of dreams to end.\n");
